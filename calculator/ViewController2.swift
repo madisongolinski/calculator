0a78e2f19994ee3fd2be9fb076bf140262fb67e6
Madison Golinski <madisongolinski@gmail.com>
2016-06-29 10:39:30 -0700

    new changes

---
+++ calculator/ViewController2.swift
@@ -0,0 +1,10 @@
+//
+//  viewController2.swift
+//  calculator
+//
+//  Created by Madison Golinski on 6/29/16.
+//  Copyright © 2016 Madison Golinski. All rights reserved.
+//
+
+import Foundation
+import UIKit
